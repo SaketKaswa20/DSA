99ea98db282a03cc79d87c3c265f3addecf1d434
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-04-18 20:12:07 +0530

    Add files via upload

---
+++ SampleProgramIfElse1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a= 2;
+  int b= 5;
+
+  if (a<b){
+    cout<<"Saket sach bol raha hai"<<endl;
+  }
+
+  else{
+    cout<<"Saket jhoot bol raha hai"<<endl;
+  }
+}
---
+++ SampleUserInput1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a,b;
+  cin>>a;
+  cin>>b;
+
+  if (a<b){
+    cout<<"Saket sach bol raha hai"<<endl;
+  }
+
+  else{
+    cout<<"Saket jhoot bol raha hai"<<endl;
+  }
+}
