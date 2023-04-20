b622f161310aa1b3ce29540dd040beb8e8a6cec9
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-04-20 20:46:56 +0530

    Add files via upload

---
+++ ProgramtoPrintNumbersusingWhileLoop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+		
+    int n;
+    cout<<"Please enter a number:";
+    cin>>n;
+    
+    while(i<=n){
+        cout<<i<<endl;
+        i=i+1;
+    }
+    
+}
