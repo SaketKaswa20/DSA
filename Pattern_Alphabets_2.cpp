5185ec262d046cdc18fc9f9d04fde13ee55d2996
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-04-29 21:25:36 +0530

    Update Pattern_Alphabets_2.cpp

--- Pattern_Alphabets_2.cpp
+++ Pattern_Alphabets_2.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+  int n;
+  cin>>n;
+
+  int row=1;
+
+  while(row<=n){
+    int column=1;
+
+    while(column<=n){
+      char ch= 'A'+column-1;
+      cout<<ch;
+      column=column+1;
+    }
+    cout<<endl;
+    row=row+1;
+  }
+}
