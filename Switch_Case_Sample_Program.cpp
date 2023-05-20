9f89053491f8e4a9979feaa7e751134dcbc481ad
Saket Kaswa <105808363+SaketKaswa20@users.noreply.github.com>
2023-05-20 21:48:12 +0530

    Create Switch_Case_Sample_Program.cpp

---
+++ Switch_Case_Sample_Program.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int num=2;
+
+  switch(2){
+    case 1: cout<<"First"<<endl;
+            break;
+
+    case 2: cout<<"Second"<<endl;
+            break;
+
+    default: cout<<"Default Case";
+  }
+}
+
+Output:
+Second
